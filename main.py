1da20063ecc437657f029d737bff922a584c539a
YangTh599 <YangTh599@northwested.org>
2024-09-09 10:13:09 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Thayer Yang
+# 09 SEP 2024
+# Printing in Python
+
+#Exercise 2-3
+
+print("Hello I'm Thayer")
+
+#Exercise 2-5
+
+print("I am Inevitable..")
+
+#Exercise 2-6
+
+print("And I am Ironman..")
